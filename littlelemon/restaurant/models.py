ebeffbf8904781a3a65c05a55240d6dadc5c1cd5
Toluwani Ajayi <toluwanijoe@gmail.com>
2026-06-27 00:34:42 +0100

    created full menu and single menu item apis

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Menu(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    menu_item_description = models.TextField(max_length=1000, default='')
+
+    def __str__(self):
+        return self.name
--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('menu/items', views.MenuItemsView.as_view()),
+    path('menu/items/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
